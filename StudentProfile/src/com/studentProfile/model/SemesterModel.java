3b7774c0e0d4d008c4135285f7a66df4aca82112
pratibind.jha@gmail.com <pratibind.jha@gmail.com@6e61f6e5-d11a-1da0-f1ee-5d7de5c5f7bc>
2013-02-04 14:36:39 +0000

    Model class for semester has been created

---
+++ StudentProfile/src/com/studentProfile/model/SemesterModel.java
@@ -0,0 +1,33 @@
+/**
+ * SemesterModel.java
+ * Date	:	4 Feb 2013 
+ */
+package com.studentProfile.model;
+
+/**
+ * @author Pratibind Kumar Jha
+ * 
+ *         Model for semester
+ */
+public class SemesterModel {
+
+	private int semID;
+	private String semName;
+
+	public int getSemID() {
+		return semID;
+	}
+
+	public void setSemID(int semID) {
+		this.semID = semID;
+	}
+
+	public String getSemName() {
+		return semName;
+	}
+
+	public void setSemName(String semName) {
+		this.semName = semName;
+	}
+
+}
