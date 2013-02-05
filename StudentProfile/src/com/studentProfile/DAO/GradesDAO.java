01d3b3c8430886ff75f6d181816899a373649404
debargha.ganguly@gmail.com <debargha.ganguly@gmail.com@6e61f6e5-d11a-1da0-f1ee-5d7de5c5f7bc>
2013-02-05 08:19:36 +0000

    GradesDAO- Specific DAO for grades page

---
+++ StudentProfile/src/com/studentProfile/DAO/GradesDAO.java
@@ -0,0 +1,121 @@
+package com.studentProfile.DAO;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import com.studentProfile.model.entity.SemesterModel;
+import com.studentProfile.model.entity.SubjectModel;
+import com.studentProfile.model.page.GradesModel;
+
+public class GradesDAO extends BaseDAO {
+	
+	
+	public ArrayList<SemesterModel> getSemesters()
+	{
+		 String query = "select semID,semName from semester";
+		 ArrayList<SemesterModel> semesters = new ArrayList<SemesterModel>();
+		 ResultSet rs=readFromDB(query);
+		 try
+		 {
+		 while(rs.next())
+		 {
+			 SemesterModel semesterModel =  new SemesterModel();
+			 semesterModel.setSemId(rs.getInt(1));
+			 semesterModel.setSemName(rs.getString(2));
+			 semesters.add(semesterModel);
+			 
+		 }
+		 }
+		 catch(Exception ex)
+		 {
+			 ex.printStackTrace();
+			 
+		 }
+		 finally
+		 {
+			 close(rs);
+		 }
+		 
+		return semesters;
+		
+	}
+	
+	public ArrayList<SubjectModel> getSubjects(int semId)
+	{
+		 String query = "select * from subject where semID="+semId;
+		 ArrayList<SubjectModel> subjects = new ArrayList<SubjectModel>();
+		 
+		 ResultSet rs=readFromDB(query);
+		 try
+		 {
+		 while(rs.next())
+		 {
+			 SubjectModel subjectModel =  new SubjectModel();
+			 subjectModel.setSubId(rs.getInt(1));
+			 subjectModel.setSubName(rs.getString(3));
+			 subjectModel.setSubCode(rs.getString(2));
+			 subjectModel.setSubSyllabus(rs.getString(4));
+			 subjects.add(subjectModel);
+		 }
+		 }
+		 catch(Exception ex)
+		 {
+			 ex.printStackTrace();
+			 
+		 }
+		 finally
+		 {
+			 close(rs);
+		 }
+		 
+		return subjects;
+		
+	}
+	
+	public ArrayList<GradesModel> getGrades(int semId,int subId,int stuId)
+	{
+		 String query = "select subName,subCode,e.enrGrade from enrollment e,subject s where s.semId="+semId;
+		 query+= " and s.subId=e.subId";
+		 query+= " and e.stuID="+stuId;
+		 if(subId!=0)
+		 {
+			 query+=" and s.subId="+subId;
+			 
+		 }
+		 System.out.println("grades query:"+query);
+		 ArrayList<GradesModel> grades = new ArrayList<GradesModel>();
+		 
+		 ResultSet rs=readFromDB(query);
+		 try
+		 {
+		 while(rs.next())
+		 {
+			
+			 GradesModel gradesModel =  new GradesModel();
+			 gradesModel.setSubject(rs.getString(1));
+			 gradesModel.setSubject_code(rs.getString(2));
+			 gradesModel.setGrade(rs.getString(3));
+			 if(!gradesModel.getGrade().contains("F"))
+				 gradesModel.setResult("PASS");
+			 else
+				 gradesModel.setResult("FAIL");
+			 grades.add(gradesModel);
+		 }
+		 }
+		 catch(Exception ex)
+		 {
+			 ex.printStackTrace();
+			 
+		 }
+		 finally
+		 {
+			 close(rs);
+		 }
+		 
+		return grades;
+		
+	}
+	
+	
+
+}
