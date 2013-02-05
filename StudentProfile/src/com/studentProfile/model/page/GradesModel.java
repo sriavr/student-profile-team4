1849ec9be2686c76155d74d7154a643948513a50
debargha.ganguly@gmail.com <debargha.ganguly@gmail.com@6e61f6e5-d11a-1da0-f1ee-5d7de5c5f7bc>
2013-02-05 08:21:18 +0000

    GradesModel -specific model for grades page

---
+++ StudentProfile/src/com/studentProfile/model/page/GradesModel.java
@@ -0,0 +1,34 @@
+package com.studentProfile.model.page;
+
+public class GradesModel {
+	
+	private String subject;
+	private String subject_code;
+	private String grade;
+	private String result;
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getSubject_code() {
+		return subject_code;
+	}
+	public void setSubject_code(String subject_code) {
+		this.subject_code = subject_code;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	public String getResult() {
+		return result;
+	}
+	public void setResult(String result) {
+		this.result = result;
+	}
+
+}
