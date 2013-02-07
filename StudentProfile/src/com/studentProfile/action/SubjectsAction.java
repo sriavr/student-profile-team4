7855551f0eee162dc1999ad0ffa4b5dacff7ad62
pratibind.jha@gmail.com <pratibind.jha@gmail.com@6e61f6e5-d11a-1da0-f1ee-5d7de5c5f7bc>
2013-02-07 18:54:58 +0000

    SubjectsAction.java has been initially added to repository.

---
+++ StudentProfile/src/com/studentProfile/action/SubjectsAction.java
@@ -0,0 +1,151 @@
+/**
+ * SubjectsAction.java
+ * Date	feb 7, 2013
+ * DEscription	:	show the subject information subject name, 
+ * 					faculty,grade,enroll and so. 
+ */
+package com.studentProfile.action;
+
+import java.util.ArrayList;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.studentProfile.DAO.SubjectsDAO;
+import com.studentProfile.model.page.SubjectsModel;
+
+/**
+ * @author Pratibind Kumar Jha
+ * 
+ */
+public class SubjectsAction extends ActionSupport {
+
+	private ArrayList<SubjectsModel> subjectsInfo;
+	private int subID;
+	private int course;
+	private String subSyllabus = "";
+	private String actionName = "";
+	private String subCode;
+
+	/**
+	 * @return the subCode
+	 */
+	public String getSubCode() {
+		return subCode;
+	}
+
+	/**
+	 * @param subCode
+	 *            the subCode to set
+	 */
+	public void setSubCode(String subCode) {
+		this.subCode = subCode;
+	}
+
+	/**
+	 * @return the actionName
+	 */
+	public String getActionName() {
+		return actionName;
+	}
+
+	/**
+	 * @param actionName
+	 *            the actionName to set
+	 */
+	public void setActionName(String actionName) {
+		this.actionName = actionName;
+	}
+
+	/**
+	 * @return the subSyllabus
+	 */
+	public String getSubSyllabus() {
+		return subSyllabus;
+	}
+
+	/**
+	 * @param subSyllabus
+	 *            the subSyllabus to set
+	 */
+	public void setSubSyllabus(String subSyllabus) {
+		this.subSyllabus = subSyllabus;
+	}
+
+	/**
+	 * @return the course
+	 */
+	public int getCourse() {
+		return course;
+	}
+
+	/**
+	 * @param course
+	 *            the course to set
+	 */
+	public void setCourse(int course) {
+		this.course = course;
+	}
+
+	/**
+	 * @return the subjectsInfo
+	 */
+	public ArrayList<SubjectsModel> getSubjectsInfo() {
+		return subjectsInfo;
+	}
+
+	/**
+	 * @param subjectsInfo
+	 *            the subjectsInfo to set
+	 */
+	public void setSubjectsInfo(ArrayList<SubjectsModel> subjectsInfo) {
+		this.subjectsInfo = subjectsInfo;
+	}
+
+	/**
+	 * @return the subID
+	 */
+	public int getSubID() {
+		return subID;
+	}
+
+	/**
+	 * @param subID
+	 *            the subID to set
+	 */
+	public void setSubID(int subID) {
+		this.subID = subID;
+	}
+
+	// show subject Info into subjetcs.jsp page.
+	public String showSubjectInfo() {
+
+		if (this.getActionName().equalsIgnoreCase("showSyllabus")) {
+			this.setActionName("subSyllabus");
+			return "success";
+		}
+		this.setActionName("showSubject");
+		int studentID;
+		// Map<String, Object> session;
+
+		// get the user information from session object.
+		studentID = 1;
+
+		/**
+		 * session = ActionContext.getContext().getSession(); String role =
+		 * (String) session.get("role");
+		 * 
+		 * if (role.equals("student")) { studentID = ((StudentModel)
+		 * session.get("user")).getStuID(); }
+		 */
+
+		this.setSubjectsInfo(SubjectsDAO.getSubjectInfo(this.getCourse(),
+				studentID));
+
+		if (this.getSubjectsInfo().isEmpty()) {
+			addActionError("");
+			addActionMessage("You have not Enroll to course");
+			return "success";
+		}
+		return "success";
+
+	}// End of showSubjectInfo().
+}// End of SubjectAction.java
