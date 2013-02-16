f36993b91041918b65c1177f374e34259d90d7a0
sriavr@gmail.com <sriavr@gmail.com@6e61f6e5-d11a-1da0-f1ee-5d7de5c5f7bc>
2013-02-16 20:52:22 +0000

    Changes done for Ganguly (GradesAction.java) and for login refinements.

---
+++ trunk/StudentProfile/WebContent/footer.jsp
@@ -0,0 +1,3 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+Developed by madcow group. &copy; 2013 International Institute of Information Technology, Bangalore
--- trunk/StudentProfile/src/com/studentProfile/action/GradesAction.java
+++ trunk/StudentProfile/src/com/studentProfile/action/GradesAction.java
@@ -2,75 +2,95 @@ package com.studentProfile.action;
 
 import java.util.ArrayList;
 
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 import com.studentProfile.DAO.GradesDAO;
 import com.studentProfile.model.entity.SemesterModel;
+import com.studentProfile.model.entity.StudentModel;
 import com.studentProfile.model.entity.SubjectModel;
 import com.studentProfile.model.page.GradesModel;
+import com.studentProfile.util.LogMessage;
 
 public class GradesAction extends ActionSupport {
-   
+
 	ArrayList<SemesterModel> semesterList = new ArrayList<SemesterModel>();
 	String userId;
+
 	public String getUserId() {
 		return userId;
 	}
+
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+
 	public ArrayList<SemesterModel> getSemesterList() {
 		return semesterList;
 	}
+
 	public void setSemesterList(ArrayList<SemesterModel> semesterList) {
 		this.semesterList = semesterList;
 	}
+
 	public ArrayList<SubjectModel> getSubjectList() {
 		return subjectList;
 	}
+
 	public void setSubjectList(ArrayList<SubjectModel> subjectList) {
 		this.subjectList = subjectList;
 	}
+
 	public ArrayList<GradesModel> getGradesList() {
 		return gradesList;
 	}
+
 	public void setGradesList(ArrayList<GradesModel> gradesList) {
 		this.gradesList = gradesList;
 	}
+
 	ArrayList<SubjectModel> subjectList = new ArrayList<SubjectModel>();
 	ArrayList<GradesModel> gradesList = new ArrayList<GradesModel>();
-	String selectedSemesterId="";
-	String selectedSubjectId="0";
-	
+	String selectedSemesterId = "";
+	String selectedSubjectId = "0";
+
 	public String getSelectedSubjectId() {
 		return selectedSubjectId;
 	}
+
 	public void setSelectedSubjectId(String selectedSubjectId) {
 		this.selectedSubjectId = selectedSubjectId;
 	}
+
 	public String getSelectedSemesterId() {
 		return selectedSemesterId;
 	}
+
 	public void setSelectedSemesterId(String selectedSemesterId) {
 		this.selectedSemesterId = selectedSemesterId;
 	}
-	public String execute()
-	{
+
+	public String execute() {
 		GradesDAO gradesDAO = new GradesDAO();
-		if(selectedSemesterId.equals(""))
-		{
-			semesterList=gradesDAO.getSemesters();
-			
-		}
-		else
-		{
-			semesterList=gradesDAO.getSemesters();
-			subjectList = gradesDAO.getSubjects(Integer.parseInt(selectedSemesterId));
-			gradesList = gradesDAO.getGrades(Integer.parseInt(selectedSemesterId), Integer.parseInt(selectedSubjectId), 2);
-			System.out.println("grade list size:"+gradesList.size());
+		if (selectedSemesterId.equals("")) {
+			semesterList = gradesDAO.getSemesters();
+
+		} else {
+			semesterList = gradesDAO.getSemesters();
+			subjectList = gradesDAO.getSubjects(Integer
+					.parseInt(selectedSemesterId));
+			StudentModel student = (StudentModel) ActionContext.getContext()
+					.getSession().get("student");
+			if (student == null || student.getStuID() == 0) {
+				return "login";
+			}
+
+			gradesList = gradesDAO.getGrades(
+					Integer.parseInt(selectedSemesterId),
+					Integer.parseInt(selectedSubjectId), student.getStuID());
+			LogMessage.log("grade list size:" + gradesList.size());
 		}
 		return SUCCESS;
-		
+
 	}
-	
-	
+
 }
