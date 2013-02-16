e29dedd98bf224647ddbdb68563886423939dc5b
yeleswarapu.pavan@gmail.com <yeleswarapu.pavan@gmail.com@6e61f6e5-d11a-1da0-f1ee-5d7de5c5f7bc>
2013-02-16 18:40:40 +0000

    MyFriendsAction- fully written code - Pavan Y

--- StudentProfile/src/com/studentProfile/action/MyFriendsAction.java
+++ StudentProfile/src/com/studentProfile/action/MyFriendsAction.java
@@ -2,10 +2,14 @@ package com.studentProfile.action;
 
 //package com.studentProfile.action;
 
+import java.io.OutputStream;
 import java.util.ArrayList;
 
+import javax.servlet.http.HttpServletResponse;
+import org.apache.struts2.ServletActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 import com.studentProfile.DAO.MyFriendsDAO;
+import com.studentProfile.DAO.StudentDAO;
 import com.studentProfile.model.entity.StudentModel;
 
 public class MyFriendsAction extends ActionSupport {
@@ -14,6 +18,14 @@ public class MyFriendsAction extends ActionSupport {
 	 * 
 	 */
 	ArrayList<StudentModel> friendsList = new ArrayList<StudentModel>();
+	private int stu_ID;
+	
+	public int getStu_ID() {
+		return stu_ID;
+	}
+	public void setStu_ID(int stu_ID) {
+		this.stu_ID = stu_ID;
+	}
 	public ArrayList<StudentModel> getFriendsList() {
 		return friendsList;
 	}
@@ -60,6 +72,9 @@ public class MyFriendsAction extends ActionSupport {
 	public void setSelectedSemesterId(String selectedSemesterId) {
 		this.selectedSemesterId = selectedSemesterId;
 	}
+	
+	HttpServletResponse response = ServletActionContext.getResponse();
+	
 	public String execute()
 	{
 		System.out.println("here.....");
@@ -71,7 +86,7 @@ public class MyFriendsAction extends ActionSupport {
 //		}
 //		else
 //		{
-			friendsList=myFriendsDAO.getFriends("1");
+			friendsList=myFriendsDAO.getFriends("2");
 //			subjectList = gradesDAO.getSubjects(Integer.parseInt(selectedSemesterId));
 //			gradesList = gradesDAO.getGrades(Integer.parseInt(selectedSemesterId), Integer.parseInt(selectedSubjectId), 2);
 //			System.out.println("grade list size:"+gradesList.size());
@@ -82,6 +97,30 @@ public class MyFriendsAction extends ActionSupport {
 	}
 	
 	
+public String retPicture(){
+		
+	StudentDAO studentDAO = new StudentDAO();
+	StudentModel curUser = new StudentModel();
+			
+	System.out.println("the picture other profile of user_id"+getStu_ID());
+		
+			curUser = studentDAO.getStudent(getStu_ID());
+		
+		response.setContentType("image/jpeg");
+		try{
+		OutputStream out = response.getOutputStream();
+		//System.out.println("here in userprofile retPicture()"+curUser.getPicData());
+		//System.out.println("here i am userprofile retPicture()"+curUser.getUser_id());
+        out.write(curUser.getStuPhoto());
+        out.close();
+		}
+		catch(Exception e){
+			System.out.println("Exception Caught in UserProfile.retPicture");
+			e.printStackTrace();
+		}
+		
+		return "success";
+	}
 }
 
 //public class MyFriendsAction {
