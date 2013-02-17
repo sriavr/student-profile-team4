a64b1d2558c3fd4f75b5e1dec0d088b7aeed41a0
yeleswarapu.pavan@gmail.com <yeleswarapu.pavan@gmail.com@6e61f6e5-d11a-1da0-f1ee-5d7de5c5f7bc>
2013-02-17 02:14:59 +0000

    -

--- StudentProfile/src/com/studentProfile/DAO/MyFriendsDAO.java
+++ StudentProfile/src/com/studentProfile/DAO/MyFriendsDAO.java
@@ -1,4 +1,5 @@
 package com.studentProfile.DAO;
+
 import java.util.ArrayList;
 import java.io.File;
 import java.io.FileInputStream;
@@ -11,51 +12,54 @@ public class MyFriendsDAO {
 	// This method validates the student credentials. If valid, returns a
 	// student object else returns null
 	ArrayList<StudentModel> friendsList = new ArrayList<StudentModel>();
-	public ArrayList<StudentModel> getFriends(String stuRollNo ) {
+
+	public ArrayList<StudentModel> getFriends(int stuID) {
 		StudentModel student = null;
 		Blob image = null;
 		int flag = -1;
 		try {
 			DatabaseUtil.connect();
-		//	LogMessage.log("Message From MyFriendsDAO.MyFriendDAO : username is "
-			//		+ student.getStuName());
+			// LogMessage.log("Message From MyFriendsDAO.MyFriendDAO : username is "
+			// + student.getStuName());
 			DatabaseUtil.ps = DatabaseUtil.con
 					.prepareStatement("(select distinct stuID,stuName,stuPhoto  from friend,student where stuID1 = ? and stuID2=stuID) UNION (select distinct stuID,stuName,stuPhoto from friend,student  where stuID2 = ? and stuID=stuID1)");
 			LogMessage
 					.log("Message From StudentDAO.login : Arguments ::username is--"
-							+ stuRollNo + " password is--");// + password);
-			DatabaseUtil.ps.setString(1, stuRollNo);
-			DatabaseUtil.ps.setString(2, stuRollNo);
+							+ stuID + " password is--");// + password);
+			DatabaseUtil.ps.setInt(1, stuID);
+			DatabaseUtil.ps.setInt(2, stuID);
 			DatabaseUtil.rs = DatabaseUtil.ps.executeQuery();
 			while (DatabaseUtil.rs.next()) {
+				flag = 1;
 				student = new StudentModel();
 				student.setStuID(DatabaseUtil.rs.getInt("stuID"));
-				//student.setIntID(DatabaseUtil.rs.getInt("intID"));
+				// student.setIntID(DatabaseUtil.rs.getInt("intID"));
 				System.out.println("stuID");
 				student.setStuName(DatabaseUtil.rs.getString("stuName"));
 				System.out.println("Stuname");
-				//to convert picture from blob byte streams
+				// to convert picture from blob byte streams
 				image = DatabaseUtil.rs.getBlob("stuPhoto");
-                student.setStuPhoto(image.getBytes(1,(int)image.length()));
-				//student.setStuPassword(DatabaseUtil.rs.getString("stuPassword"));
-				//student.setStuDOB(DatabaseUtil.rs.getDate("stuDOB"));
+				System.out.println("image length:" + image.length());
+				student.setStuPhoto(image.getBytes(1, (int) image.length()));
+				// student.setStuPassword(DatabaseUtil.rs.getString("stuPassword"));
+				// student.setStuDOB(DatabaseUtil.rs.getDate("stuDOB"));
 				// ---------------- Yet to be
 				// implemented-------------------------------
 				// student.setStuLoggedIn(DatabaseUtil.rs.getTime("stuLoggedIn"));
 				// student.setStuPhoto(DatabaseUtil.rs.getBlob("stuPhoto"));
 				// ----------------
 				// ----------------------------------------------------
-				//student.setStuRollNo(stuRollNo);
+				// student.setStuRollNo(stuRollNo);
 				friendsList.add(student);
-				
+
 			}
 		} catch (Exception e) {
 			LogMessage.log("Exception Caught in MyfriendsDAO.getFriends");
 			e.printStackTrace();
-			student = null;
+			friendsList = null;
 		} finally {
 			if (flag != 1)
-				student = null;
+				friendsList = null;
 			DatabaseUtil.connectionClose();
 		}
 
